56a9706ca06c88542fd5de36d81d86792fddc32f
rvosa <rutgeraldo@gmail.com>
2026-09-20 11:39:00 +0200

    uses command line argument

--- scripts/tp1_translate_frames.py
+++ scripts/tp1_translate_frames.py
@@ -1,9 +1,7 @@
+import sys
 from Bio import SeqIO
 
-record = next(SeqIO.parse("out/Danaus.COI-5P.fas", "fasta"), None)
-if record is None:
-    raise SystemExit("No FASTA records found in out/Danaus.COI-5P.fas")
-
+record = next(SeqIO.parse(sys.argv[1], "fasta"))
 for frame in range(3):
     protein = record.seq[frame:].translate(table=5)   # invertebrate mitochondrial
     print(frame, protein.count("*"), protein[:40])
