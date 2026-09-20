d6d7b0dec0ebd557cfed01d059166619b27947d6
rvosa <rutgeraldo@gmail.com>
2026-09-20 11:22:17 +0200

    aligned with markdown

--- scripts/filter_marker.py
+++ scripts/filter_marker.py
@@ -1,12 +1,10 @@
 import sys
 from Bio import SeqIO
 
-if len(sys.argv) != 3:
-    raise SystemExit(f"Usage: {sys.argv[0]} <input.fasta> <marker>")
-
 infile, marker = sys.argv[1], sys.argv[2]
 
 for record in SeqIO.parse(infile, "fasta"):
     fields = record.description.split('|')
-    if len(fields) > 2 and fields[-2] == marker:
-        SeqIO.write(record, sys.stdout, "fasta")
+    if len(fields) > 2 and fields[2] == marker:
+        print('>' + record.description)
+        print(record.seq)
