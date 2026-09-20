abc69c284056d45eb8131beb0f6ab5d68992863c
rvosa <rutgeraldo@gmail.com>
2026-09-20 11:47:52 +0200

    computes all frames for all records

--- scripts/tp1_translate_frames.py
+++ scripts/tp1_translate_frames.py
@@ -1,7 +1,19 @@
 import sys
 from Bio import SeqIO
+from Bio.Data.CodonTable import TranslationError
 
-record = next(SeqIO.parse(sys.argv[1], "fasta"))
-for frame in range(3):
-    protein = record.seq[frame:].translate(table=5)   # invertebrate mitochondrial
-    print(frame, protein.count("*"), protein[:40])
+print("id\tframe\tstops")
+for record in SeqIO.parse(sys.argv[1], "fasta"):
+    counts = []
+    for frame in range(3):
+        seq = record.seq[frame:]
+        stops = 0
+        for i in range(0, len(seq) - len(seq) % 3, 3):
+            try:
+                if seq[i:i + 3].translate(table=5) == "*":
+                    stops += 1
+            except TranslationError:
+                pass
+        counts.append((stops, frame))
+    stops, frame = min(counts)
+    print(f"{record.id}\t{frame}\t{stops}")
